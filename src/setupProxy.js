0ea5b552931a47cf76e061a33f92b29ed654344c
laurent papinaud <laurent.papinaud@gmail.com>
2026-01-23 11:47:36 +0100

    change local target due to WSL-Windows setup on Laurent laptop

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://localhost:7035',
+      target: 'http://192.168.1.156:5293',
       changeOrigin: true,
-      secure: false,
-      // Par défaut, http-proxy-middleware retire le préfixe /api
-      // Si votre backend écoute sur /api/spectacles, décommentez la ligne suivante:
-      // pathRewrite: { '^/api': '/api' },
-      // Si votre backend écoute directement sur /spectacles (sans /api), laissez tel quel
     })
-  )
-}
-
+  );
+};
